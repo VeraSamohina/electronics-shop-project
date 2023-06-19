32516cf1a496df11adcd68864b00777a036c13cf
Vera Samohina <samohinavera44@gmail.com>
2023-06-19 19:46:15 +0300

    Add class Item in item.py, add tests in test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    assert Item('Диск', 100, 3).calculate_total_price() == 300
+
+
+def test_apply_discount():
+    Item.pay_rate = 0.5
+    item3 = Item('Книга', 400, 1)
+    item3.apply_discount()
+    assert item3.price == 200
