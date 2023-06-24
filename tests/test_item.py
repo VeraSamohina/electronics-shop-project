535b713260ab70c6c89dcbc23618198f1648230b
Vera Samohina <samohinavera44@gmail.com>
2023-06-24 19:42:25 +0300

    Add tests for new methods

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
+import pytest
 
 
 def test_calculate_total_price():
@@ -11,3 +12,25 @@ def test_apply_discount():
     item3 = Item('Книга', 400, 1)
     item3.apply_discount()
     assert item3.price == 200
+
+
+def test_string_to_number():
+    item5 = Item("Устройство", '90', '8')
+    item6 = Item("Блок", '90.3', '8')
+    assert item5.string_to_number(item5.price) == 90
+    assert item6.string_to_number(item6.price) == 90
+
+
+def test_item_name():
+    item = Item('Телефон', 10000, 5)
+    item.name = "Монитор"
+    assert item.name == "Монитор"
+    item.name = "Жидкокристаллический"
+    with pytest.raises(AssertionError):
+        assert item.name == "Жидкокристаллический"
+    assert item.name == "Жидкокрист"
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()  # создание объектов из данных файла
+    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
