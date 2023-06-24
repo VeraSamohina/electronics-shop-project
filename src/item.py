bcd87ed3242d881d6ede805296ce6c28a4cb454a
Vera Samohina <samohinavera44@gmail.com>
2023-06-24 19:42:00 +0300

    Add classmethod, staticmethod instantiate_from_csv string_to_number in items.py

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,35 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            print('Длина наименования товара превышает 10 символов')
+            self.__name = name[:10]
+
+    @staticmethod
+    def string_to_number(value):
+        value = int(float(value))
+        return value
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        Item.all.clear()
+        with open('../src/items.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile, fieldnames=None, restkey=None, restval=None, dialect='excel')
+            for row in reader:
+                item = Item(row['name'], Item.string_to_number(row['price']), Item.string_to_number(row['quantity']))
 
     def calculate_total_price(self) -> float:
         """
